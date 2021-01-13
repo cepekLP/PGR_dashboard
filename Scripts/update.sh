3637c1985b8a01d4a6c58f29f23bcd5cf35b6bbc
cepekLP <cepeklp@gmail.com>
2021-01-13 15:50:02 +0100

    update directory to run.py

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,3 +1,6 @@
 git pull https://e7f552bd4e460d68a22840dbfd9aa40d23791583@github.com/cepekLP/PGR_dashboard
+cd /home/pi/Documents/PGR_dashboard
 python3 run.py
 
+#sudo crontab -e
+#@reboot /home/pi/Documents/PGR_dashboard/Scripts/update.sh
