7f06c3271cac637cfc64bbfc057680f6ff1f5e26
cepekLP <cepeklp@gmail.com>
2021-01-30 18:13:40 +0100

    set acl

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir -p "/home/pi/Documents/PGR_dashboard/log"
+mkdir -p /home/pi/Documents/PGR_dashboard/log
+sudo setfacl -m u:pi:rwx log
 sudo apt-get install python3-pyqt5
 sudo apt install python3-gpiozero
 sudo echo '
