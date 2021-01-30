7d70b39ead58437b6d01c49e6e8aa15839e07bf7
cepekLP <cepeklp@gmail.com>
2021-01-30 17:56:43 +0100

    modified Sripts to UTF-8 without BOM

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -1,3 +1,7 @@
-﻿mkdir /home/pi/Documents/PGR_dashboard/log
+#!/bin/bash
+mkdir -p "/home/pi/Documents/PGR_dashboard/log"
 sudo apt-get install python3-pyqt5
+sudo apt install python3-gpiozero
+sudo echo '
+@sh /home/pi/Documents/PGR_dashboard/Scripts/start.sh' >> /etc/xdg/lxsession/LXDE-pi/autostart
 sudo reboot
--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd Documents/PGR_dashboard
 python3 run.py
