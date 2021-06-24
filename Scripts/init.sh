6fa0dae6f50a6b4e839a9be296cd88a829e834bc
Marcin Nadarzynski <m.nadarzynski01@gmail.com>
2021-06-24 22:19:28 +0200

    Faster boot optimalization

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -24,12 +24,27 @@ rclone_config(){
   LIST=$(rclone listremotes)
   echo "*/30 * * * * rclone copy Documents/PGR_dashboard/log/ {$LIST}PGRacing/log" >> etc/crontab
 }
+
+fast_start(){
+  sudo systemctl disable dphys-swapfile.service
+  sudo systemctl disable apt-daily.service
+  sudo systemctl disable hciuart.service
+  sudo systemctl disable raspi-config.service
+  sudo systemctl disable avahi-daemon.service
+  sudo systemctl disable triggerhappy.service
+  sudo systemctl disable systemd-udev-trigger.service
+  sudo systemctl disable apt-daily-upgrade.timer
+
+  sudo cp /home/pi/Documents/PGR_dashboard/raspberrypi/config.txt /boot/config.txt
+  sudo cp /home/pi/Documents/PGR_dashboard/raspberrypi/cmdline.txt /boot/cmdline.txt
+
+}
 main(){
   mkdir -p /home/pi/Documents/PGR_dashboard/log
   setfacl -m u:pi:rwx log
 
   apt install -y python3-pyqt5 python3-gpiozero rclone
-  pip3 install rpi_ws281x
+  #pip3 install rpi_ws281x
 
   set_up_autostart
   set_up_wifi
--- Scripts/start.sh
+++ Scripts/start.sh
@@ -3,4 +3,7 @@ cd /home/pi/Documents/PGR_dashboard
 if [[ -f /log/Points.json ]]; then
   cp /log/Points.json /Points.json
 fi
-sudo python3 run.py
+
+sudo python3 run.py &
+sleep 0.2
+exec /sbin/init
