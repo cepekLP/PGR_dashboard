d19b87b80d432ee1a08891c8d331235b6310f7fd
Marcin Nadarzynski <m.nadarzynski01@gmail.com>
2021-07-15 16:51:08 +0200

    Add rest of infos

--- Workers/Server/ajax.js
+++ Workers/Server/ajax.js
@@ -25,6 +25,18 @@ function parse(data_){
             var data = JSON.parse(data_);
             document.getElementById("rpm").innerHTML= data["rpm"];
             document.getElementById("water_temp").innerHTML= data["water_temp"];
+            document.getElementById("gear").innerHTML= data["gear"];
+            document.getElementById("gear_cut").innerHTML= data["gear_cut"];
+            document.getElementById("TPS").innerHTML= data["TPS"];
+            document.getElementById("oil_temp").innerHTML= data["oil_temp"];
+            document.getElementById("voltage").innerHTML= data["voltage"];
+            document.getElementById("MAP").innerHTML= data["MAP"];
+            document.getElementById("intake_temp").innerHTML= data["intake_temp"];
+            document.getElementById("lambda").innerHTML= data["lambda"];
+            document.getElementById("ecu_temp").innerHTML= data["ecu_temp"];
+            document.getElementById("oil_press").innerHTML= data["oil_press"];
+            document.getElementById("rpi_temp").innerHTML= data["rpi_temp"];
+            document.getElementById("fuel_press").innerHTML= data["fuel_press"];
         }
     }
     catch (error) {
